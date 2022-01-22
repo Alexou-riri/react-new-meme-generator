6b139efc3804b2c2a91d1294957c6857425e131d
Alex <alexandra.rieux@gmx.com>
2022-01-22 19:57:04 +0100

    new function

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [memes, setMemes] = useState([]);
+  const [value, setValue] = useState('');
 
   useEffect(() => {
-    axios.get('https://api.memegen.link/images').then((res) => {
+    axios.get('https://api.memegen.link/templates').then((res) => {
       setMemes(res.data);
     });
   }, []);
 
+  const selectMeme = `https://api.memegen.link/images/${value}/`;
+
   return (
     <div>
-      {memes.map((meme) => (
-        <ul key={meme.id}>
-          <li value={meme.id}>
-            <img src={meme.url} alt="un_meme" />
-          </li>
-        </ul>
-      ))}
+      <img src={selectMeme} alt="un_meme" />
+      <select
+        onChange={() => {
+          const changeMeme = document.getElementsById();
+        }}
+      >
+        {memes.map((meme) => (
+          <option key={meme.id} value={meme.id}>
+            {meme.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
