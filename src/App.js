e8d8bd2a94ac6dc27f01be10508d224e31c606f3
Alex <alexandra.rieux@gmx.com>
2022-02-28 23:51:55 +0100

    Insert new const and new onCHange function for selecting meme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [memes, setMemes] = useState([]);
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState('aag');
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
 
   useEffect(() => {
     axios.get('https://api.memegen.link/templates').then((res) => {
@@ -12,23 +14,48 @@ function App() {
     });
   }, []);
 
-  const selectMeme = `https://api.memegen.link/images/${value}/`;
+  const createMeme = `https://api.memegen.link/images/${value}/${topText}/${bottomText}`;
+
+  const onChangeValue = (e) => {
+    setValue(e.currentTarget.value);
+  };
+  const onChangeTopText = (e) => {
+    setTopText(e.currentTarget.value);
+  };
+
+  const onChangeBottomText = (e) => {
+    setBottomText(e.currentTarget.value);
+  };
 
   return (
-    <div>
-      <img src={selectMeme} alt="un_meme" />
-      <select
-        onChange={() => {
-          const changeMeme = document.getElementsById();
-        }}
-      >
-        {memes.map((meme) => (
-          <option key={meme.id} value={meme.id}>
-            {meme.name}
-          </option>
-        ))}
-      </select>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          {/* Header display only elements*/}
+          <h1>Meme generator</h1>
+          <h2> Have fun ! </h2>
+        </header>
+      </div>
+      <div className="MemeGenerator">{/* <MemeGenerator /> */}</div>
+      <div>
+        <select
+          onChange={onChangeValue}
+          // const changeMeme = document.getElementsById();
+          // const selectedValue = changeMeme.value;
+          // setValue(selectedValue);
+          // }}
+        >
+          {memes.map((meme) => {
+            return (
+              <option key={meme.id} value={meme.id}>
+                {meme.name}
+              </option>
+            );
+          })}
+        </select>
+        <img src={createMeme} alt="un_meme" />
+      </div>
+    </>
   );
 }
 
